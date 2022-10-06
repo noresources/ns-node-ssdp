e55b1102042830c7dec0815d8e36d243fd332561
Renaud Guillard <dev@nore.fr>
2022-10-06 20:30:15 +0200

    Project configuration utility updated

--- tools/update-package-info.mjs
+++ tools/update-package-info.mjs
@@ -1,10 +1,28 @@
+/*
+ * Copyright © 2022 by Renaud Guillard (dev@nore.fr)
+ * Distributed under the terms of the MIT License, see LICENSE
+ */
+ 
+'use strict';
+
 import * as fs from 'fs';
 
 const info = JSON.parse (fs.readFileSync ('package.json').toString());
-const text = 'const PACKAGE = {\n'
-	+ '	\'NAME\': \''+info.name+'\',\n'
-	+ '	\'VERSION\': \''+info.version+'\'\n'
-	+ '};\n'
-	+ 'export default PACKAGE;\n';
+const text = `
+/*
+ * Copyright © 2022 by Renaud Guillard (dev@nore.fr)
+ * Distributed under the terms of the MIT License, see LICENSE
+ */
+ 
+'use strict';
+
+const PACKAGE = {
+		"NAME": "${info.name}",
+		"VERSION": "${info.version}"
 	
+};
+export { PACKAGE };
+export default PACKAGE;
+`;
+
 fs.writeFileSync('src/Package.mjs', text);
